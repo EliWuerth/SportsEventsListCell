cbfe2f036c6735b6c8d071b164e533045f6d8f65
EliWuerth <emwcd7@umsystem.edu>
2024-09-18 15:21:27 -0500

    updated code

--- SportsEventsListCell/ContentView.swift
+++ SportsEventsListCell/ContentView.swift
@@ -1,24 +1,172 @@
-//
-//  ContentView.swift
-//  SportsEventsListCell
-//
-//  Created by Eli Wuerth on 9/18/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            HStack(alignment: .center) {
+                Image("Basketball")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("Basketball")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("soccer")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                    .aspectRatio(contentMode: .fit)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("soccer")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("football")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("football")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("football")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("Baseball")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("Baseball")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("hockey")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
+            HStack(alignment: .center) {
+                Image("hockey")
+                    .resizable()
+                    .frame(width: 24, height: 24)
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Portland at Orlando")
+                        .font(.body)
+                    Text("Feb 23, 2017 at 10:00 AM")
+                        .font(.caption)
+                }
+                Spacer()
+                Image(systemName: "chevron.right")
+            }
         }
-        .padding()
+
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().navigationTitle("Sports Events")
     }
 }
 
-#Preview {
-    ContentView()
-}

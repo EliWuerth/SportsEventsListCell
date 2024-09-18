b8d1bda0379dcd65d5c29ccb3e9182a12184b447
Eli Wuerth <emwcd7@umsystem.edu>
2024-09-18 12:41:16 -0500

    Initial Commit

---
+++ SportsEventsListCell/SportsEventsListCellApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportsEventsListCellApp.swift
+//  SportsEventsListCell
+//
+//  Created by Eli Wuerth on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SportsEventsListCellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
